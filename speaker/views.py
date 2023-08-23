b6d32c74e5eb03893767acbc295bccc93299db05
Eric Ndungutse <dav.ndungutse@gmail.com>
2023-08-23 20:31:58 +0300

    document speaker api

--- speaker/views.py
+++ speaker/views.py
@@ -4,9 +4,23 @@ from rest_framework.response import Response
 from .serializers import SpeakerSerializer
 from .models import Speaker
 import json
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
+@swagger_auto_schema(method='post', request_body=openapi.Schema(
+    type=openapi.TYPE_OBJECT, 
+    properties={
+        'name': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+        'biography': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+        'photo': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+        'email': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+        'phone_number': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+        'linkedin': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+        'twitter': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+        'instagram': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+    }
+))
 @csrf_exempt
-
 @api_view(['GET', 'POST'])
 def speakers(request):
     if request.method == 'GET':
@@ -21,6 +35,19 @@ def speakers(request):
             return Response(speaker.data, status=201)
         return Response(speaker.errors)
 
+@swagger_auto_schema(method='patch', request_body=openapi.Schema(
+    type=openapi.TYPE_OBJECT, 
+    properties={
+        'name': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+        'biography': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+        'photo': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+        'email': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+        'phone_number': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+        'linkedin': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+        'twitter': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+        'instagram': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+    }
+))
 @api_view(['GET', 'DELETE', 'PATCH'])
 def get_speaker(request, id):
     # GET SPEAKER
