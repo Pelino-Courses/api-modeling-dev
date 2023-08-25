215f8fcfd1f154f30ec49bb5c4ef02b0dde8c746
Eric Ndungutse <dav.ndungutse@gmail.com>
2023-08-25 22:53:59 +0300

    prepare deployment

---
+++ participant/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from event.models import Event
+# Create your models here.
+class Participant(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    events = models.ManyToManyField(Event)
+
+    class Meta:
+        verbose_name = 'Participant'
+        verbose_name_plural = 'Participants'
+
+    def __str__(self):
+        return self.name
